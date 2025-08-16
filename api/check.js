15023c82d23394da09dd90d0c1e946ed36b3ec06
mksir12 <116490353+mksir12@users.noreply.github.com>
2025-08-16 16:05:33 +0530

    Create check.js

---
+++ api/check.js
@@ -0,0 +1,70 @@
+export default async function handler(req, res) {
+  const videos = [
+    "https://i.imgur.com/wVHPACz.mp4",
+  "https://i.imgur.com/NErSHY5.mp4",
+  "https://i.imgur.com/CUZf2Cf.mp4",
+  "https://i.imgur.com/WqckfnX.mp4",
+  "https://i.imgur.com/Z2cwsrz.mp4",
+  "https://i.imgur.com/5gL4cZo.mp4",
+  "https://i.imgur.com/ZLkPIsY.mp4",
+  "https://i.imgur.com/kLedgdR.mp4",
+  "https://i.imgur.com/KUGRx8b.mp4",
+  "https://i.imgur.com/h1UKkcP.mp4",
+  "https://i.imgur.com/nWJdaGX.mp4",
+  "https://i.imgur.com/vdxn09c.mp4",
+  "https://i.imgur.com/fb1VAlr.mp4",
+  "https://i.imgur.com/bheqlD7.mp4",
+  "https://i.imgur.com/uWhr4iL.mp4",
+  "https://i.imgur.com/zGzFerK.mp4",
+  "https://i.imgur.com/3Cv02cz.mp4",
+  "https://i.imgur.com/LHW3T60.mp4",
+  "https://i.imgur.com/IUtst5x.mp4",
+  "https://i.imgur.com/M4rX4PB.mp4",
+  "https://i.imgur.com/sO3pNoF.mp4",
+  "https://i.imgur.com/RHTIEKB.mp4",
+  "https://i.imgur.com/P7NEpNd.mp4",
+  "https://i.imgur.com/k4HVHC9.mp4",
+  "https://i.imgur.com/gnd5eGE.mp4",
+  "https://i.imgur.com/JBoRyy6.mp4",
+  "https://i.imgur.com/aF8x8zb.mp4",
+  "https://i.imgur.com/3rNFcgE.mp4",
+  "https://i.imgur.com/9ec6N3l.mp4",
+  "https://i.imgur.com/iERiqLB.mp4",
+  "https://i.imgur.com/WK62Btn.mp4",
+  "https://i.imgur.com/PsbcOF9.mp4",
+  "https://i.imgur.com/0Ysn9Ol.mp4",
+  "https://i.imgur.com/1D4a8Fj.mp4",
+  "https://i.imgur.com/MuKrzQQ.mp4",
+  "https://i.imgur.com/IX6iMFV.mp4",
+  "https://i.imgur.com/B4zr1Uq.mp4"
+  ];
+
+  const working = [];
+  const broken = [];
+
+  for (const url of videos) {
+    try {
+      const response = await fetch(url, { method: "HEAD" });
+
+      if (response.ok && response.headers.get("content-type")?.includes("video")) {
+        working.push(url);
+      } else {
+        broken.push(`${url} ${response.status}`);
+      }
+    } catch (err) {
+      broken.push(`${url} ERROR`);
+    }
+  }
+
+  // Build plain text response like your example
+  let output = "";
+  if (working.length > 0) {
+    output += "✅ WORKING:\n" + working.join("\n") + "\n\n";
+  }
+  if (broken.length > 0) {
+    output += "❌ BROKEN:\n" + broken.join("\n") + "\n";
+  }
+
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send(output.trim());
+}
